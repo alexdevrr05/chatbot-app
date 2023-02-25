259eb0def5c222053764c583fae734a0781d5789
ismael <ismael.romeromorales.21@gmail.com>
2023-02-25 13:31:16 -0600

    added custom button

--- src/screens/ConcursosScreen.js
+++ src/screens/ConcursosScreen.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import CustomButtom from '../components/CustomButton';
 
 const ColaboradoresScreen = () => {
   const { top } = useSafeAreaInsets();
   return (
     <View style={{ marginTop: top + 20 }}>
       <Text>ColaboradoresScreen</Text>
+      <CustomButtom text='Agregar colaborador'/>
     </View>
   );
 };
