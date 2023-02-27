9193833a48322bfa8d3674da0fac825ead7e5af7
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2023-02-27 08:41:31 -0600

    Added three more screens

--- src/screens/PresentadoresScreen.js
+++ src/screens/PresentadoresScreen.js
@@ -1,13 +1,224 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { View, Text } from 'react-native';
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
+
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { globalStyles } from '../constants/theme';
+import { useForm } from '../hooks/useForm';
 
-const PresentadoresScreen = () => {
+import CustomButton from '../components/CustomButton';
+
+const PresentadoresScreen = ({ navigation }) => {
   const { top } = useSafeAreaInsets();
+  const [showError, setShowError] = useState('');
+
+  const { onChange, onReset, statecurrent } = useForm({
+    apeMat: '',
+    apePat: '',
+    clave: '',
+    correo: '',
+    direccion: '',
+    empresa: '',
+    especialidad: '',
+    grado: '',
+    nombre: '',
+    telefono: '',
+  });
+  const {
+    nombre,
+    apePat,
+    apeMat,
+    correo,
+    direccion,
+    telefono,
+    grado,
+    empresa,
+    especialidad,
+    clave,
+  } = statecurrent;
+
+  const isValidFields = () => {
+    if (
+      telefono === '' ||
+      nombre === '' ||
+      grado === '' ||
+      especialidad === '' ||
+      empresa === '' ||
+      direccion === '' ||
+      correo === '' ||
+      clave === '' ||
+      apePat === '' ||
+      apeMat === ''
+    )
+      return false;
+    return true;
+  };
+
   return (
-    <View style={{ marginTop: top + 20 }}>
-      <Text>PresentadoresScreen</Text>
+    <View
+      style={{
+        ...globalStyles.globalBackground,
+        ...globalStyles.globalMargin,
+        marginTop: top + 20,
+      }}
+    >
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          {/* OnFocus input */}
+          <View
+            style={{
+              ...globalStyles.globalMargin,
+              backgroundColor: 'white',
+              justifyContent: 'center',
+              paddingTop: 5,
+            }}
+          >
+            <Text
+              style={{
+                ...globalStyles.title,
+                fontWeight: 'bold',
+                textAlign: 'center',
+              }}
+            >
+              Presentadores
+            </Text>
+            <TextInput
+              style={{ ...globalStyles.input, color: 'black' }}
+              onChangeText={(value) => onChange(value, 'nombre')}
+              placeholder='Nombre'
+              placeholderTextColor='gray'
+              autoCorrect={false}
+              value={nombre}
+              autoCapitalize='none'
+              keyboardType='text'
+            />
+            <TextInput
+              style={{ ...globalStyles.input, color: 'black' }}
+              onChangeText={(value) => onChange(value, 'apePat')}
+              placeholder='ApePat'
+              placeholderTextColor='gray'
+              autoCorrect={false}
+              value={apePat}
+              autoCapitalize='none'
+              keyboardType='text'
+            />
+            <TextInput
+              style={{ ...globalStyles.input, color: 'black' }}
+              onChangeText={(value) => onChange(value, 'apeMat')}
+              placeholder='ApeMat'
+              placeholderTextColor='gray'
+              autoCorrect={false}
+              value={apeMat}
+              autoCapitalize='none'
+              keyboardType='text'
+            />
+            <TextInput
+              style={{ ...globalStyles.input, color: 'black' }}
+              onChangeText={(value) => onChange(value, 'correo')}
+              placeholder='Correo'
+              placeholderTextColor='gray'
+              autoCorrect={false}
+              value={correo}
+              autoCapitalize='none'
+              keyboardType='text'
+            />
+            <TextInput
+              style={{ ...globalStyles.input, color: 'black' }}
+              onChangeText={(value) => onChange(value, 'direccion')}
+              placeholder='Direccion'
+              placeholderTextColor='gray'
+              autoCorrect={false}
+              value={direccion}
+              autoCapitalize='none'
+              keyboardType='text'
+            />
+            <TextInput
+              style={{ ...globalStyles.input, color: 'black' }}
+              onChangeText={(value) => onChange(value, 'telefono')}
+              placeholder='Telefono'
+              placeholderTextColor='gray'
+              autoCorrect={false}
+              value={telefono}
+              autoCapitalize='none'
+              keyboardType='text'
+            />
+            <TextInput
+              style={{ ...globalStyles.input, color: 'black' }}
+              onChangeText={(value) => onChange(value, 'grado')}
+              placeholder='Grado'
+              placeholderTextColor='gray'
+              autoCorrect={false}
+              value={grado}
+              autoCapitalize='none'
+              keyboardType='text'
+            />
+            <TextInput
+              style={{ ...globalStyles.input, color: 'black' }}
+              onChangeText={(value) => onChange(value, 'empresa')}
+              placeholder='Empresa'
+              placeholderTextColor='gray'
+              autoCorrect={false}
+              value={empresa}
+              autoCapitalize='none'
+              keyboardType='text'
+            />
+            <TextInput
+              style={{ ...globalStyles.input, color: 'black' }}
+              onChangeText={(value) => onChange(value, 'especialidad')}
+              placeholder='Especialidad'
+              placeholderTextColor='gray'
+              autoCorrect={false}
+              value={especialidad}
+              autoCapitalize='none'
+              keyboardType='text'
+            />
+            <TextInput
+              style={{ ...globalStyles.input, color: 'black' }}
+              onChangeText={(value) => onChange(value, 'clave')}
+              placeholder='Clave'
+              placeholderTextColor='gray'
+              autoCorrect={false}
+              value={clave}
+              autoCapitalize='none'
+              keyboardType='text'
+            />
+          </View>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+
+      {/* Contenedor forgot pass & signIn button */}
+      <View
+        style={{
+          ...globalStyles.globalMargin,
+          width: '100%',
+          alignItems: 'center',
+        }}
+      >
+        <Text style={{ color: 'red' }}>{showError !== '' && showError}</Text>
+
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+          <TouchableOpacity
+            disabled={!isValidFields()}
+            onPress={() =>
+              // onClickSignIn()
+              console.log('hello world from my new comment')
+            }
+          >
+            <CustomButton enable={!isValidFields()} text='Crear presentador' />
+          </TouchableOpacity>
+        </TouchableWithoutFeedback>
+      </View>
     </View>
   );
 };
--- src/screens/TalleresScreen.js
+++ src/screens/TalleresScreen.js
@@ -1,13 +1,210 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { View, Text } from 'react-native';
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
+
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { globalStyles } from '../constants/theme';
+import { useForm } from '../hooks/useForm';
 
-const TalleresScreen = () => {
+import CustomButton from '../components/CustomButton';
+
+const TalleresScreen = ({ navigation }) => {
   const { top } = useSafeAreaInsets();
+  const [showError, setShowError] = useState('');
+
+  const { onChange, onReset, statecurrent } = useForm({
+    talNombre: '',
+    eveClave: '',
+    talClave: '',
+    talCupo: '',
+    talDias: '',
+    talStatus: '',
+    talFechaInico: '',
+    talFechaFin: '',
+    talHorario: '',
+  });
+  const {
+    talNombre,
+    eveClave,
+    talClave,
+    talCupo,
+    talDias,
+    talStatus,
+    talFechaInico,
+    talFechaFin,
+    talHorario,
+  } = statecurrent;
+
+  const isValidFields = () => {
+    if (
+      talNombre === '' ||
+      eveClave === '' ||
+      talClave === '' ||
+      talCupo === '' ||
+      talDias === '' ||
+      talStatus === '' ||
+      talFechaInico === '' ||
+      talFechaFin === '' ||
+      talHorario === ''
+    )
+      return false;
+    return true;
+  };
+
   return (
-    <View style={{ marginTop: top + 20 }}>
-      <Text>TalleresScreen</Text>
+    <View
+      style={{
+        ...globalStyles.globalBackground,
+        ...globalStyles.globalMargin,
+        marginTop: top + 20,
+      }}
+    >
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View
+            style={{
+              ...globalStyles.globalMargin,
+              backgroundColor: 'white',
+              justifyContent: 'center',
+              paddingTop: 5,
+            }}
+          >
+            <Text
+              style={{
+                ...globalStyles.title,
+                fontWeight: 'bold',
+                textAlign: 'center',
+              }}
+            >
+              Talleres
+            </Text>
+            <TextInput
+              style={{ ...globalStyles.input, color: 'black' }}
+              onChangeText={(value) => onChange(value, 'talNombre')}
+              placeholder='Nombre'
+              placeholderTextColor='gray'
+              autoCorrect={false}
+              value={talNombre}
+              autoCapitalize='none'
+              keyboardType='text'
+            />
+            <TextInput
+              style={{ ...globalStyles.input, color: 'black' }}
+              onChangeText={(value) => onChange(value, 'eveClave')}
+              placeholder='Clave'
+              placeholderTextColor='gray'
+              autoCorrect={false}
+              value={eveClave}
+              autoCapitalize='none'
+              keyboardType='text'
+            />
+            <TextInput
+              style={{ ...globalStyles.input, color: 'black' }}
+              onChangeText={(value) => onChange(value, 'talClave')}
+              placeholder='Clave'
+              placeholderTextColor='gray'
+              autoCorrect={false}
+              value={talClave}
+              autoCapitalize='none'
+              keyboardType='text'
+            />
+            <TextInput
+              style={{ ...globalStyles.input, color: 'black' }}
+              onChangeText={(value) => onChange(value, 'talCupo')}
+              placeholder='Cupo'
+              placeholderTextColor='gray'
+              autoCorrect={false}
+              value={talCupo}
+              autoCapitalize='none'
+              keyboardType='text'
+            />
+            <TextInput
+              style={{ ...globalStyles.input, color: 'black' }}
+              onChangeText={(value) => onChange(value, 'talDias')}
+              placeholder='Dias'
+              placeholderTextColor='gray'
+              autoCorrect={false}
+              value={talDias}
+              autoCapitalize='none'
+              keyboardType='text'
+            />
+            <TextInput
+              style={{ ...globalStyles.input, color: 'black' }}
+              onChangeText={(value) => onChange(value, 'talStatus')}
+              placeholder='Status'
+              placeholderTextColor='gray'
+              autoCorrect={false}
+              value={talStatus}
+              autoCapitalize='none'
+              keyboardType='text'
+            />
+            <TextInput
+              style={{ ...globalStyles.input, color: 'black' }}
+              onChangeText={(value) => onChange(value, 'talFechaInico')}
+              placeholder='FechaInico'
+              placeholderTextColor='gray'
+              autoCorrect={false}
+              value={talFechaInico}
+              autoCapitalize='none'
+              keyboardType='text'
+            />
+            <TextInput
+              style={{ ...globalStyles.input, color: 'black' }}
+              onChangeText={(value) => onChange(value, 'talFechaFin')}
+              placeholder='FechaFin'
+              placeholderTextColor='gray'
+              autoCorrect={false}
+              value={talFechaFin}
+              autoCapitalize='none'
+              keyboardType='text'
+            />
+            <TextInput
+              style={{ ...globalStyles.input, color: 'black' }}
+              onChangeText={(value) => onChange(value, 'talHorario')}
+              placeholder='Horario'
+              placeholderTextColor='gray'
+              autoCorrect={false}
+              value={talHorario}
+              autoCapitalize='none'
+              keyboardType='text'
+            />
+          </View>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+
+      {/* Contenedor forgot pass & signIn button */}
+      <View
+        style={{
+          ...globalStyles.globalMargin,
+          width: '100%',
+          alignItems: 'center',
+        }}
+      >
+        <Text style={{ color: 'red' }}>{showError !== '' && showError}</Text>
+
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+          <TouchableOpacity
+            disabled={!isValidFields()}
+            onPress={() =>
+              // onClickSignIn()
+              console.log('hello world from Talleres ')
+            }
+          >
+            <CustomButton enable={!isValidFields()} text='Crear taller' />
+          </TouchableOpacity>
+        </TouchableWithoutFeedback>
+      </View>
     </View>
   );
 };
