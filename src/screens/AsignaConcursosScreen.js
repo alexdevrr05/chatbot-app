f2d0621217ed1f3e5a6f8c9f55391b00ccbfb756
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2023-03-03 07:53:59 -0600

    Screens finished

--- src/screens/AsignaConcursosScreen.js
+++ src/screens/AsignaConcursosScreen.js
@@ -1,12 +1,153 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import React, { useState } from 'react';
 
-const AsignaConcursosScreen = () => {
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
+
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { globalStyles } from '../constants/theme';
+import { useForm } from '../hooks/useForm';
+
+import CustomButton from '../components/CustomButton';
+
+const AsignaConcursosScreen = ({ navigation }) => {
   const { top } = useSafeAreaInsets();
+  const [showError, setShowError] = useState('');
+
+  const { onChange, onReset, statecurrent } = useForm({
+    acesor: '',
+    clave: '',
+    concursoId: '',
+    participanteId: '',
+    rol: '',
+  });
+  const { rol, participanteId, concursoId, clave, acesor } = statecurrent;
+
+  const isValidFields = () => {
+    if (
+      acesor === '' ||
+      clave === '' ||
+      concursoId === '' ||
+      participanteId === '' ||
+      rol === ''
+    )
+      return false;
+    return true;
+  };
+
   return (
-    <View style={{ marginTop: top + 20 }}>
-      <Text>AsignaConcursosScreen</Text>
+    <View
+      style={{
+        ...globalStyles.globalBackground,
+        ...globalStyles.globalMargin,
+        marginTop: top + 20,
+      }}
+    >
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          {/* OnFocus input */}
+          <View
+            style={{
+              ...globalStyles.globalMargin,
+              backgroundColor: 'white',
+              justifyContent: 'center',
+              paddingTop: 5,
+            }}
+          >
+            <Text
+              style={{
+                ...globalStyles.title,
+                fontWeight: 'bold',
+                textAlign: 'center',
+              }}
+            >
+              Asignación de concursos
+            </Text>
+            <TextInput
+              style={{ ...globalStyles.input, color: 'black' }}
+              onChangeText={(value) => onChange(value, 'acesor')}
+              placeholder='Acesor'
+              placeholderTextColor='gray'
+              autoCorrect={false}
+              value={acesor}
+              autoCapitalize='none'
+              keyboardType='text'
+            />
+            <TextInput
+              style={{ ...globalStyles.input, color: 'black' }}
+              onChangeText={(value) => onChange(value, 'clave')}
+              placeholder='Clave'
+              placeholderTextColor='gray'
+              autoCorrect={false}
+              value={clave}
+              autoCapitalize='none'
+              keyboardType='text'
+            />
+            <TextInput
+              style={{ ...globalStyles.input, color: 'black' }}
+              onChangeText={(value) => onChange(value, 'concursoId')}
+              placeholder='ConcursoId'
+              placeholderTextColor='gray'
+              autoCorrect={false}
+              value={concursoId}
+              autoCapitalize='none'
+              keyboardType='text'
+            />
+            <TextInput
+              style={{ ...globalStyles.input, color: 'black' }}
+              onChangeText={(value) => onChange(value, 'participanteId')}
+              placeholder='ParticipanteId'
+              placeholderTextColor='gray'
+              autoCorrect={false}
+              value={participanteId}
+              autoCapitalize='none'
+              keyboardType='text'
+            />
+            <TextInput
+              style={{ ...globalStyles.input, color: 'black' }}
+              onChangeText={(value) => onChange(value, 'rol')}
+              placeholder='Rol'
+              placeholderTextColor='gray'
+              autoCorrect={false}
+              value={rol}
+              autoCapitalize='none'
+              keyboardType='text'
+            />
+          </View>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+
+      {/* Contenedor forgot pass & signIn button */}
+      <View
+        style={{
+          ...globalStyles.globalMargin,
+          width: '100%',
+          alignItems: 'center',
+        }}
+      >
+        <Text style={{ color: 'red' }}>{showError !== '' && showError}</Text>
+
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+          <TouchableOpacity
+            disabled={!isValidFields()}
+            onPress={() =>
+              // onClickSignIn()
+              console.log('Asignar concurso comment')
+            }
+          >
+            <CustomButton enable={!isValidFields()} text='Asignar concurso' />
+          </TouchableOpacity>
+        </TouchableWithoutFeedback>
+      </View>
     </View>
   );
 };
--- src/screens/AsignaTallConfScreen.js
+++ src/screens/AsignaTallConfScreen.js
@@ -1,13 +1,157 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { View, Text } from 'react-native';
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
+
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { globalStyles } from '../constants/theme';
+import { useForm } from '../hooks/useForm';
 
-const AsignaTallConfScreen = () => {
+import CustomButton from '../components/CustomButton';
+
+const AsignaTallConfScreen = ({ navigation }) => {
   const { top } = useSafeAreaInsets();
+  const [showError, setShowError] = useState('');
+
+  const { onChange, onReset, statecurrent } = useForm({
+    clave: '',
+    colaboradorId: '',
+    conferenciaId: '',
+    lugar: '',
+    presentadorId: '',
+  });
+  const { clave, colaboradorId, conferenciaId, lugar, presentadorId } =
+    statecurrent;
+
+  const isValidFields = () => {
+    if (
+      clave === '' ||
+      colaboradorId === '' ||
+      conferenciaId === '' ||
+      lugar === '' ||
+      presentadorId === ''
+    )
+      return false;
+    return true;
+  };
+
   return (
-    <View style={{ marginTop: top + 20 }}>
-      <Text>AsignaTallConfScreen</Text>
+    <View
+      style={{
+        ...globalStyles.globalBackground,
+        ...globalStyles.globalMargin,
+        marginTop: top + 20,
+      }}
+    >
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          {/* OnFocus input */}
+          <View
+            style={{
+              ...globalStyles.globalMargin,
+              backgroundColor: 'white',
+              justifyContent: 'center',
+              paddingTop: 5,
+            }}
+          >
+            <Text
+              style={{
+                ...globalStyles.title,
+                fontWeight: 'bold',
+                textAlign: 'center',
+              }}
+            >
+              Asignación de talleres de conferencias
+            </Text>
+            <TextInput
+              style={{ ...globalStyles.input, color: 'black' }}
+              onChangeText={(value) => onChange(value, 'clave')}
+              placeholder='Clave'
+              placeholderTextColor='gray'
+              autoCorrect={false}
+              value={clave}
+              autoCapitalize='none'
+              keyboardType='text'
+            />
+            <TextInput
+              style={{ ...globalStyles.input, color: 'black' }}
+              onChangeText={(value) => onChange(value, 'colaboradorId')}
+              placeholder='ColaboradorId'
+              placeholderTextColor='gray'
+              autoCorrect={false}
+              value={colaboradorId}
+              autoCapitalize='none'
+              keyboardType='text'
+            />
+            <TextInput
+              style={{ ...globalStyles.input, color: 'black' }}
+              onChangeText={(value) => onChange(value, 'conferenciaId')}
+              placeholder='ConferenciaId'
+              placeholderTextColor='gray'
+              autoCorrect={false}
+              value={conferenciaId}
+              autoCapitalize='none'
+              keyboardType='text'
+            />
+            <TextInput
+              style={{ ...globalStyles.input, color: 'black' }}
+              onChangeText={(value) => onChange(value, 'lugar')}
+              placeholder='Lugar'
+              placeholderTextColor='gray'
+              autoCorrect={false}
+              value={lugar}
+              autoCapitalize='none'
+              keyboardType='text'
+            />
+            <TextInput
+              style={{ ...globalStyles.input, color: 'black' }}
+              onChangeText={(value) => onChange(value, 'presentadorId')}
+              placeholder='PresentadorId'
+              placeholderTextColor='gray'
+              autoCorrect={false}
+              value={presentadorId}
+              autoCapitalize='none'
+              keyboardType='text'
+            />
+          </View>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+
+      {/* Contenedor forgot pass & signIn button */}
+      <View
+        style={{
+          ...globalStyles.globalMargin,
+          width: '100%',
+          alignItems: 'center',
+        }}
+      >
+        <Text style={{ color: 'red' }}>{showError !== '' && showError}</Text>
+
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+          <TouchableOpacity
+            disabled={!isValidFields()}
+            onPress={() =>
+              // onClickSignIn()
+              console.log('Asignar concurso comment')
+            }
+          >
+            <CustomButton
+              enable={!isValidFields()}
+              text='Asignar taller de conferencias'
+            />
+          </TouchableOpacity>
+        </TouchableWithoutFeedback>
+      </View>
     </View>
   );
 };
